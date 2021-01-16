3c72a515f0ab3fc0c5361c832993df03a335d571
Jonathan Rivera <johny24rivera@outlook.com>
2021-01-16 01:13:39 -0700

    pushhing changes in ta02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,47 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta02', { 
-        title: 'Team Activity 02', 
+let users = []
+
+router.get('/', (req, res, next) => {
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
+        users: users,
     });
+});
+
+router.post('/addUser', (req, res, next) => {
+    let name = req.body.fname + req.body.surname;
+    const userExists = users.filter((user) => {
+        console.log('in func' + user.firstName + user.lastName)
+        return user.firstName + user.lastName === name;
+    });
+
+    if (userExists == undefined || userExists.length == 0) {
+        users.push({
+            firstName: req.body.fname,
+            lastName: req.body.surname,
+        });
+    };
+
+    console.log(users)
+    res.redirect('/ta02')
+});
+
+router.post('/deleteUser', (req, res, next) => {
+    let name = req.body.fname + req.body.surname;
+    let size = users.length;
+
+    users = users.filter((user) => {
+        console.log('in func' + user.firstName + user.lastName)
+        return user.firstName + user.lastName != name;
+    });
+
+    console.log(users)
+    res.redirect('/ta02')
 });
 
 module.exports = router;
