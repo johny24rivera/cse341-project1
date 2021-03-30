f6d0a40e2d72420d53ace09f3f0b6555699b8802
Jonathan Rivera <johny24rivera@outlook.com>
2021-03-29 21:56:10 -0600

    heroku errors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session')
-const PORT = process.env.PORT || 5000 // So we can run on heroku || (OR) localhost:5000
+const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 
 const app = express();
 
